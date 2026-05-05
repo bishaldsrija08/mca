a4db81f704c3d0a8ed1d73a53fc69f5fc7fc81b4
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-05-05 22:59:35 +0545

    strings done

---
+++ First Semester/Python/strings/str.py
@@ -0,0 +1,17 @@
+fruit = "banana"
+print(fruit)
+print(fruit[0])
+print(len(fruit))
+print(fruit[-1])
+
+i = 0
+while i < len(fruit):
+    print(fruit[i])
+    i = i + 1
+    
+    
+for each in fruit:
+    print(each)
+    
+
+fruit[0]= "B"
